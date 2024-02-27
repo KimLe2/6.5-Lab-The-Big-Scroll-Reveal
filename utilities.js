312c61cc8ddb1061a162ae248fe02395c5a81ad7
KimLe2 <156560646+KimLe2@users.noreply.github.com>
2024-02-27 02:19:35 +0000

    changes

--- utilities.js
+++ utilities.js
@@ -1,10 +1,34 @@
+ScrollReveal().reveal('.record', {opacity:0.8, duration:1000, delay:1000});
+
+ScrollReveal().reveal('.house', {distance:'500px', delay:1500});
+
+ScrollReveal().reveal('.swan', {duration:2000, delay:2000});
+
+ScrollReveal().reveal('.canyon', {delay: 3000, easing:'ease-out', duration:3000,});
+
+ScrollReveal().reveal('.park', {scale: 0.85, easing:'ease-in', delay:3500});
+
+/* window.onload = function() {
+    ScrollReveal().reveal('site', {delay: 1000}
+}
 
 function clickOMatic() {
     alert('Hello!');
   }
+
+  window.onload = function() {
+
+  };
+
+const click = document.getElementById('click');
+
+click.addEventListener("click", (event) => {
+  clickOMatic();
+});
 
   let clickOMatic = function() {
     alert('Hello!');
   };
 
   let clickOMatic = () => alert('Hello!');
+  */
